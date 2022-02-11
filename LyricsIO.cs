68dfec68c5b2f01627080a061dccb259cdf6c0c7
tryashtar <tryashtar@gmail.com>
2022-02-10 23:29:54 -0700

    for FLAC, "lyrics" is synced, "unsynced lyrics" is not

--- LyricsIO.cs
+++ LyricsIO.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using TagLib;
 using TagLib.Id3v2;
+using TryashtarUtils.Utility;
 using File = System.IO.File;
 using Tag = TagLib.Tag;
 
@@ -12,19 +13,20 @@ namespace TryashtarUtils.Music
 {
     public static class LyricsIO
     {
-        public const string OGG_LYRICS = "SYNCED LYRICS";
+        public const string OGG_LYRICS = "LYRICS";
+        public const string OGG_UNSYNCED_LYRICS = "UNSYNCED LYRICS";
         public static bool ToFile(TagLib.File file, Lyrics? lyrics)
         {
             bool changed = false;
             var simple = lyrics?.ToSimple();
-            changed |= file.Tag.Lyrics != simple;
-            file.Tag.Lyrics = simple;
 
             var id3v2 = (TagLib.Id3v2.Tag)file.GetTag(TagTypes.Id3v2);
             var ogg = (TagLib.Ogg.XiphComment)file.GetTag(TagTypes.Xiph);
 
             if (id3v2 != null)
             {
+                changed |= id3v2.Lyrics != simple;
+                id3v2.Lyrics = simple;
                 var existing_frames = id3v2.GetFrames<SynchronisedLyricsFrame>().ToList();
                 foreach (var frame in existing_frames)
                 {
@@ -52,14 +54,19 @@ namespace TryashtarUtils.Music
                 {
                     changed |= ogg.GetFirstField(OGG_LYRICS) != null;
                     ogg.RemoveField(OGG_LYRICS);
+                    changed |= ogg.GetFirstField(OGG_UNSYNCED_LYRICS) != null;
+                    ogg.RemoveField(OGG_UNSYNCED_LYRICS);
                 }
                 else
                 {
-                    var lrc = lyrics.ToLrc();
+                    var lrc = String.Join("\n", lyrics.ToLrc());
                     var existing = ogg.GetField(OGG_LYRICS);
-                    changed |= !existing.SequenceEqual(lrc);
+                    changed |= existing.Length != 1 || existing[0] != lrc;
                     ogg.SetField(OGG_LYRICS, lrc);
 
+                    var existing_unsynced = ogg.GetField(OGG_UNSYNCED_LYRICS);
+                    changed |= existing_unsynced.Length != 1 || existing_unsynced[0] != simple;
+                    ogg.SetField(OGG_UNSYNCED_LYRICS, simple);
                 }
             }
             return changed;
@@ -106,9 +113,38 @@ namespace TryashtarUtils.Music
 
         public static Lyrics? FromXiph(TagLib.Ogg.XiphComment tag)
         {
-            var lyrics = tag.GetField(OGG_LYRICS);
-            if (lyrics != null && lyrics.Length > 0)
-                return FromLrc(lyrics);
+            var text = tag.GetFirstField(OGG_LYRICS);
+            if (text != null)
+            {
+                var lyrics = StringUtils.SplitLines(text).ToArray();
+                if (TryFromLrc(lyrics, out var result))
+                    return result;
+                else
+                    return new Lyrics(String.Join("\n", lyrics));
+            }
+            var unsynced = tag.GetFirstField(OGG_UNSYNCED_LYRICS);
+            if (unsynced != null)
+                return new Lyrics(String.Join("\n", unsynced));
+            return null;
+        }
+
+        private static bool TryFromLrc(string[] lines, out Lyrics? result)
+        {
+            result = null;
+            if (lines.Length > 0 && FromLrcLine(lines[0]) == null)
+                return false;
+            result = FromLrc(lines);
+            return true;
+        }
+
+        private static LyricsEntry? FromLrcLine(string line)
+        {
+            var match = SharedIO.LrcRegex.Match(line);
+            if (match.Success)
+            {
+                if (TimeSpan.TryParseExact(match.Groups["time"].Value, SharedIO.TimespanFormats, null, out var time))
+                    return new LyricsEntry(match.Groups["line"].Value, time);
+            }
             return null;
         }
 
@@ -117,12 +153,9 @@ namespace TryashtarUtils.Music
             var list = new List<LyricsEntry>();
             foreach (var line in lines)
             {
-                var match = SharedIO.LrcRegex.Match(line);
-                if (match.Success)
-                {
-                    if (TimeSpan.TryParseExact(match.Groups["time"].Value, SharedIO.TimespanFormats, null, out var time))
-                        list.Add(new LyricsEntry(match.Groups["line"].Value, time));
-                }
+                var entry = FromLrcLine(line);
+                if (entry != null)
+                    list.Add(entry.Value);
             }
             if (list.Count == 0)
                 return null;
