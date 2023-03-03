70ca3704cf45a50fb8b7ee58bfd10733c767bc61
tryashtar <tryashtar@gmail.com>
2023-03-02 21:44:10 -0700

    Add flac images

--- PictureExtensions.cs
+++ PictureExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using TagLib;
 using TagLib.Id3v2;
@@ -9,6 +10,11 @@ namespace TryashtarUtils.Music
         public static IPicture? Get(TagLib.Id3v2.Tag tag)
         {
             return tag.GetFrames<AttachmentFrame>().FirstOrDefault();
+        }
+
+        public static IPicture? Get(TagLib.Flac.Metadata tag)
+        {
+            return tag.Pictures.FirstOrDefault();
         }
 
         public static bool Set(TagLib.Id3v2.Tag tag, IPicture? picture)
@@ -24,6 +30,20 @@ namespace TryashtarUtils.Music
             var attachment = new AttachmentFrame(picture);
             tag.AddFrame(attachment);
             return existing.Count != 1 || existing[0].Data == picture.Data;
+        }
+
+        public static bool Set(TagLib.Flac.Metadata tag, IPicture? picture)
+        {
+            var existing = tag.Pictures;
+
+            if (picture == null)
+            {
+                tag.Pictures = Array.Empty<IPicture>();
+                return existing.Length > 0;
+            }
+
+            tag.Pictures = new[] { picture };
+            return existing.Length != 1 || existing[0].Data == picture.Data;
         }
     }
 }
