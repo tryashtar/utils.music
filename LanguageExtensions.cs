b619abd5b2a39142c53c66b7c57a2a93863898c8
tryashtar <tryashtar@gmail.com>
2023-07-05 14:25:58 -0600

    Improve language extensions

--- LanguageExtensions.cs
+++ LanguageExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
+using System.Text;
 using TagLib;
 using TagLib.Id3v2;
 
@@ -9,44 +11,57 @@ namespace TryashtarUtils.Music
         public const string ID3_LANGUAGE_TAG = "TLAN";
         public const string XIPH_LANGUAGE_TAG = "LANGUAGE";
 
-        public static string? Get(TagLib.Id3v2.Tag tag)
+        public static string? GetId3v2(TagLib.Id3v2.Tag tag)
         {
-            foreach (var frame in tag.GetFrames<TextInformationFrame>().ToList())
-            {
-                if (frame.FrameId.ToString() == ID3_LANGUAGE_TAG)
-                {
-                    if (frame.Text.Length > 0)
-                        return frame.Text[0];
-                }
-            }
-            return null;
+            return tag.GetFrames<TextInformationFrame>()
+                .FirstOrDefault(x => x.FrameId.ToString() == ID3_LANGUAGE_TAG && x.Text.Length > 0)?.Text[0];
         }
 
-        public static string? Get(TagLib.Ogg.XiphComment tag)
+        public static string? GetXiph(TagLib.Ogg.XiphComment tag)
         {
             return tag.GetFirstField(XIPH_LANGUAGE_TAG);
         }
 
-        public static void Set(TagLib.Id3v2.Tag tag, string? value)
+        public static bool SetId3v2(TagLib.Id3v2.Tag tag, string? value)
         {
-            foreach (var frame in tag.GetFrames<TextInformationFrame>().ToList())
-            {
-                if (frame.FrameId.ToString() == ID3_LANGUAGE_TAG)
-                    tag.RemoveFrame(frame);
-            }
+            var existing = tag.GetFrames<TextInformationFrame>().Where(x => x.FrameId.ToString() == ID3_LANGUAGE_TAG)
+                .ToList();
             if (value != null)
             {
                 var lang = new TextInformationFrame(ByteVector.FromString(ID3_LANGUAGE_TAG, StringType.UTF8))
                 {
-                    Text = new[] { value }
+                    Text = new[] { value },
+                    TextEncoding = StringType.UTF16
                 };
+                if (existing.Count == 1 && existing[0].Render(4) == lang.Render(4))
+                    return false;
+                foreach (var frame in existing)
+                {
+                    tag.RemoveFrame(frame);
+                }
+
                 tag.AddFrame(lang);
+                return true;
+            }
+            else
+            {
+                foreach (var frame in existing)
+                {
+                    tag.RemoveFrame(frame);
+                }
+
+                return existing.Count > 0;
             }
         }
 
-        public static void Set(TagLib.Ogg.XiphComment tag, string? value)
+        public static bool SetXiph(TagLib.Ogg.XiphComment tag, string? value)
         {
+            var existing = tag.GetField(XIPH_LANGUAGE_TAG);
             tag.SetField(XIPH_LANGUAGE_TAG, value);
+            if (value == null)
+                return existing.Length != 0;
+            else
+                return existing.Length != 1 || existing[0] != value;
         }
     }
 }
